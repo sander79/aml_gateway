a4b0e5fa80f4fc2fad3499cd3a3fb86ffc1dc08b
Harry <Harry@host.docker.internal>
2022-11-09 12:23:30 +0100

    refactoring

--- src/main/java/it/sander/aml/AmlGateway.java
+++ src/main/java/it/sander/aml/AmlGateway.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class AmlGateway {
 
 	
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(AmlGateway.class, args);
 	}
 
 }
